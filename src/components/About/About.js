6608cb6d86d6f5d75e747261e34415ca51f6d7aa
Neo Müller <neo.mueller@swisscom.com>
2023-04-01 22:09:42 +0200

    feat: add try new styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,9 +3,8 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Particle from '../Particle';
 import Techstack from './Techstack';
 import Aboutcard from './AboutCard';
-import laptopImg from '../../Assets/about.png';
 import Toolstack from './Toolstack';
-
+import AboutPageBild from '../../Assets/about.png';
 function About() {
   return (
     <Container fluid className="about-section">
@@ -30,7 +29,7 @@ function About() {
             style={{ paddingTop: '120px', paddingBottom: '50px' }}
             className="about-img"
           >
-            <img src={laptopImg} alt="about" className="img-fluid" />
+            <img src={AboutPageBild} alt="about" className="img-fluid" />
           </Col>
         </Row>
         <h1 className="project-heading">
